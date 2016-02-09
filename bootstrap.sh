ba8e456bd862ff938b40bd3eaccb69df37dbae62
Eric Wu <ericwuyi@gmail.com>
2016-02-08 23:07:29 -0800

    Clearly label the stages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Checking preconditions..."
+### Check preconditions
+
+echo "About to check preconditions..."
+sleep 3s
 
 # This should be run by the root user
 if [[ "${EUID}" -ne 0 ]]; then
@@ -10,21 +13,26 @@ fi
 
 echo "Done checking preconditions."
 
-cd
+### Non-interactive
 
 export DEBIAN_FRONTEND=noninteractive
 
-# Add '/usr/local/bin' to root's $PATH variable.
-export PATH="${PATH}:/usr/local/bin"
+### Update and upgrade
 
-# Upgrade
-echo "Updating and upgrading system."
+echo "About to update and upgrade the system..."
+sleep 3s
+
 apt-get -q -y update
 apt-get -q -y dist-upgrade
 apt-get -q -y autoremove
 apt-get -q -y autoclean
 
-echo "Installing basic packages."
+echo "Done updating and upgrading the system."
+
+### Various packages, tools for development
+
+echo "About to install various packages, tools for development..."
+sleep 3s
 
 # Additional fonts
 apt-get -q -y install fonts-droid fonts-inconsolata
@@ -48,18 +56,31 @@ apt-get -q -y install curl wget
 
 apt-get -q -y install vim
 
+# Handy command-line tool for handling CSV files based on OCaml
+apt-get -q -y install csvtool
+
+apt-get -q -y install ruby-dev ruby
+
+echo "Done installing packages, tools for development."
+
+## Git
+
+echo "About to install git..."
+sleep 3s
+
 apt-get -q -y install git
 apt-get -q -y install libgnome-keyring-dev
 cd /usr/share/doc/git/contrib/credential/gnome-keyring/
 make
 cd
 
-# Handy command-line tool for handling CSV files based on OCaml
-apt-get -q -y install csvtool
+echo "Done installing git."
 
-apt-get -q -y install ruby-dev ruby
+### Python
 
-echo "Installing python packages."
+echo "About to install Python..."
+sleep 3s
+
 # Python header files
 apt-get -q -y install python-dev python3-dev
 # Linear algebra packages needed by numpy and scipy
@@ -82,6 +103,9 @@ apt-get -q -y install python python3
 apt-get -q -y install python-pip python3-pip
 # virtualenv
 apt-get -q -y install virtualenv
+# aws command-line
+apt-get -q -y install awscli
+# Use virtualenvs for SciPy stacks
 # SciPy stack for Python 2
 # apt-get -q -y install ipython ipython-notebook
 # apt-get -q -y install python-numpy python-matplotlib python-scipy
@@ -91,15 +115,22 @@ apt-get -q -y install virtualenv
 # apt-get -q -y install ipython3 ipython3-notebook
 # apt-get -q -y install python3-numpy python3-matplotlib python3-scipy
 # apt-get -q -y install python3-pandas
-apt-get -q -y install awscli
 
-# java
+echo "Done installing python."
+
+### java
+
+echo "About to install java..."
+sleep 3s
+
 echo "Installing open jdk 7 and 8."
 apt-get -q -y install openjdk-7-jdk openjdk-7-source
 apt-get -q -y -t jessie-backports install openjdk-8-jdk openjdk-8-source
 
-# clean up
-echo "Cleaning up."
+echo "Done installing java."
+
+### Clean up
+
 apt-get -q -y update
 apt-get -q -y dist-upgrade
 apt-get -q -y autoremove
